8d23b84061398e49057ff8d5ff11c9ce26c6c2f5
dimagural <lakimon13@gmail.com>
2017-05-17 14:51:49 +0300

    begin changes

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,56 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-    ],
+   'aliases' => [
+      '@userPhoto'  => '@frontend/web/uploads/users',
+   ],
+   'timeZone'  => 'Europe/Kiev',
+   'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+   'components' => [
+      'i18n' => [
+         'translations' => [
+            '*' => [
+               'class' => 'yii\i18n\PhpMessageSource',
+               'basePath' => '@common/messages',
+            ],
+         ],
+      ],
+      'log' => [
+         'traceLevel' => YII_DEBUG ? 3 : 0,
+         'targets' => [
+            [
+               'class' => 'yii\log\FileTarget',
+               'levels' => ['error', 'warning'],
+            ],
+         ],
+      ],
+      'cache' => [
+         'class' => 'yii\caching\FileCache',
+         'cachePath' => '@backend/runtime/cache',
+      ],
+      'authManager' => [
+         'class' => 'common\components\AccessControl\DbManagerCs',  // Используем кастомные RBAC manager из пакета AccessControl
+         'cache' => 'cache' //Включаем кеширование
+      ],
+//      'mailer' => [
+//         'class' => 'yii\swiftmailer\Mailer',
+//         'useFileTransport' => false,
+//         'transport' => [
+//            'class' => 'Swift_SmtpTransport',
+//            'host' => 'smtp.yandex.ru',
+//            'username' => 'tuito-ru@yandex.ru',
+//            'password' => '19841328',
+//            'port' => '465',
+//            'encryption' => 'ssl',
+//         ],
+//         'viewPath' => '@common/mail',
+//      ],
+      'accessControl' => [
+         'class' => 'common\components\AccessControl\Main',
+         'branchs' => [
+            ['b', '@backend/controllers', 'Админ панель', '\backend\controllers'],
+            ['f', '@frontend/controllers','Публичная часть', '\frontend\controllers'],
+         ],
+      ],
+   ]
+
 ];
